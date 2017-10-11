78b2dff251f15fae9aeab17d1cce2540e2737cd0
System Administrator <root@makedeMacBook-Pro.local>
2017-10-11 18:02:39 +0800

    dashBoard

--- src/dashBoard.js
+++ src/dashBoard.js
@@ -18,7 +18,8 @@ export default (content,width=400,height=400,maxNum=100,unit='%',title='仪表�
   const r = (width>height?height:width)/2
 
 
-  var createRadian = (index,color) => svg.append('path')
+  var createRadian = (index,color) => {
+    svg.append('path')
       .attr('d',d3.arc().innerRadius(r-10)
           .outerRadius(r)
           .startAngle(sr+index*st)
@@ -26,20 +27,21 @@ export default (content,width=400,height=400,maxNum=100,unit='%',title='仪表�
       )
       .attr('fill',color)
 
-  var greyPath = createRadian(0,'grey')
-  var greenPath = createRadian(1,'green')
-  var redPath = createRadian(2,'red')
-
-  for(var i=0;i<mn+1;i++){
-    var corner = sr+st*3/(mn)*i-st
-    svg.append('rect')
-      .attr('width',10)
-      .attr('height',3)
-      .attr('fill','#000')
-      .attr('x',(r-15)*cos(corner))
-      .attr('y',(r-15)*sin(corner))
-      .style('transform',`rotate(${corner/pi*180-180}deg)`)
-      .style('transform-origin','50% 50%')
+    var d = 6
+    for(var i=0;i<d+1;i++){
+      var corner = sr+st/d*i+index*st-st
+      svg.append('rect')
+        .attr('width',10)
+        .attr('height',3)
+        .attr('fill',color)
+        .attr('x',(r-15)*cos(corner))
+        .attr('y',(r-15)*sin(corner))
+        .style('transform',`rotate(${corner/pi*180-180}deg)`)
+        .style('transform-origin','50% 50%')
+    }
   }
+  createRadian(0,'grey')
+  createRadian(1,'green')
+  createRadian(2,'red')
 
 }
