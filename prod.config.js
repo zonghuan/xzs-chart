8d8a689337c70978d5671a4ac0332bf33818ddc8
System Administrator <root@makedeMacBook-Pro.local>
2017-10-12 17:00:08 +0800

    npm publish

---
+++ prod.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry:{
+    'chart':'./src/index.js'
+  },
+  externals:{
+    'd3':"require('d3')"
+  },
+  output:{
+    filename:'chart.js',
+    path:__dirname
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins:[
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  ]
+}
